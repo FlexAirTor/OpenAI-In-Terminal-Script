892c30615c83ad2ee89a64b60fadca7dbdee74c8
FlexAirTor <82037355+FlexAirTor@users.noreply.github.com>
2022-12-16 14:33:57 -0600

    Add files via upload

---
+++ openai_prueba_v0.3.py
@@ -0,0 +1,23 @@
+import openai
+import sys
+
+# Ejemplo de ejecucion de script
+# python3 nombre_del_archivo.py "¿Cuál es la capital de Francia?"
+
+
+openai.api_key = "Write_your_API-Key_Here"
+
+# Leer la pregunta de la línea de comandos
+prompt = sys.argv[1]
+
+# Hacer una pregunta al modelo
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1024,
+    temperature=0.5,
+)
+
+# Imprimir la respuesta del modelo
+print(response["choices"][0]["text"])
+
